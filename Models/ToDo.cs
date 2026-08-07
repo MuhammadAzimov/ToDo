2b1fc82353602413d660d6d1750c1c57d848af1c
Muhammad Azimov <muhammadazimov09990@gmail.com>
2026-08-07 11:40:32 +0500

    add prop

--- Models/ToDo.cs
+++ Models/ToDo.cs
@@ -4,6 +4,7 @@ public class ToDo
 {
     public int id{get;set;}
     public string Title{get;set;}
-    public string Description{get;set;}
+    public string Deescription{get;set;}
+    
     public bool IsCompleted{get;set;}
 } 
